2e6350ac1a3ac74cb5d76dbd495bce161f4cf510
Turakar <turakar23@github.com>
2025-05-07 17:02:37 +0200

    Update versions, compile triton from source

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p flash-attention/dist
 mkdir -p flash-attention/hopper/dist
 
 if [[ "$1" == "--flash-attn3" ]]; then
-    (cd flash-attention/hopper; MAX_JOBS=16 uv run python setup.py bdist_wheel)
+    (cd flash-attention/hopper; MAX_JOBS=16 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
 else
-    (cd flash-attention; MAX_JOBS=16 uv run python setup.py bdist_wheel)
+    (cd flash-attention; MAX_JOBS=64 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
 fi
