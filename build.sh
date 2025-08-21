ba882b3f02438a1631ecfd7c30fd584ae6044d1b
Turakar <turakar23@gmail.com>
2025-08-21 16:05:32 +0200

    Delete build directories before build

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ export FLASH_ATTENTION_FORCE_BUILD=TRUE
 export FLASH_ATTN_CUDA_ARCHS="80;90"
 
 mkdir -p flash-attention/dist
+rm -rf flash-attention/build/*
 mkdir -p flash-attention/hopper/dist
+rm -rf flash-attention/hopper/build/*
 
 if [[ "$1" == "--flash-attn3" ]]; then
     (cd flash-attention/hopper; MAX_JOBS=8 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
