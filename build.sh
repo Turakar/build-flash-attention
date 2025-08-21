7346602c38bb5ce767cbfa65c17d23516b01550d
Turakar <turakar23@gmail.com>
2025-08-21 15:29:38 +0200

    Adapt to GH200

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+set -e
+
 uv sync
 
 export FLASH_ATTENTION_FORCE_BUILD=TRUE
+export FLASH_ATTN_CUDA_ARCHS="80;90"
 
 mkdir -p flash-attention/dist
 mkdir -p flash-attention/hopper/dist
 
 if [[ "$1" == "--flash-attn3" ]]; then
-    (cd flash-attention/hopper; MAX_JOBS=16 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
+    (cd flash-attention/hopper; MAX_JOBS=8 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
 else
-    (cd flash-attention; MAX_JOBS=16 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
+    (cd flash-attention; MAX_JOBS=8 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
 fi
