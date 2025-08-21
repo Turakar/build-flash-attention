891f56cc4cd8cfc1826fbf53ee703b2663fa9f41
Turakar <turakar23@gmail.com>
2025-08-21 17:20:55 +0200

    Small fixes to build script

--- build.sh
+++ build.sh
@@ -6,15 +6,14 @@ uv sync
 uv run python -c 'import torch; print(f"torch version: {torch.__version__}\ntorch cuda version: {torch.version.cuda}")'
 
 export FLASH_ATTENTION_FORCE_BUILD=TRUE
-export FLASH_ATTN_CUDA_ARCHS="80;90"
 
 mkdir -p flash-attention/dist
-rm -rf flash-attention/build/*
 mkdir -p flash-attention/hopper/dist
-rm -rf flash-attention/hopper/build/*
 
 if [[ "$1" == "--flash-attn3" ]]; then
-    (cd flash-attention/hopper; MAX_JOBS=8 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
+    rm -rf flash-attention/hopper/build/*
+    (cd flash-attention/hopper; uv run python setup.py sdist; MAX_JOBS=8 uv run python setup.py bdist_wheel; )
 else
-    (cd flash-attention; MAX_JOBS=8 uv run python setup.py bdist_wheel; uv run python setup.py sdist)
+    rm -rf flash-attention/build/*
+    (cd flash-attention; uv run python setup.py sdist; MAX_JOBS=8 FLASH_ATTN_CUDA_ARCHS="80;90" uv run python setup.py bdist_wheel)
 fi
