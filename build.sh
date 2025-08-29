5d89db4998b3c8857a2098f48806f71f073b389d
Turakar <turakar23@gmail.com>
2025-08-29 16:22:39 +0200

    Disable some FA3 features to reduce compile time

--- build.sh
+++ build.sh
@@ -13,9 +13,17 @@ mkdir -p flash-attention/hopper/dist
 if [[ "$1" == "--flash-attn-3" ]]; then
     rm -rf flash-attention/hopper/build/*
     # hopper compilation is extremely memory hungry, up to 22 GB per job (1 job = 1 thread)
-    (cd flash-attention/hopper; uv run python setup.py sdist; MAX_JOBS=4 uv run python setup.py bdist_wheel)
+    (
+        cd flash-attention/hopper;
+        uv run python setup.py sdist;
+        MAX_JOBS=4 FLASH_ATTENTION_DISABLE_SM80=TRUE FLASH_ATTENTION_DISABLE_FP16=TRUE FLASH_ATTENTION_DISABLE_FP8=TRUE uv run python setup.py bdist_wheel
+    )
 else
     rm -rf flash-attention/build/*
     # effectively 16 threads, 8 jobs * 2 GPU architectures
-    (cd flash-attention; uv run python setup.py sdist; MAX_JOBS=8 FLASH_ATTN_CUDA_ARCHS="80;90" uv run python setup.py bdist_wheel)
+    (
+        cd flash-attention;
+        uv run python setup.py sdist;
+        MAX_JOBS=8 FLASH_ATTN_CUDA_ARCHS="80;90" uv run python setup.py bdist_wheel
+    )
 fi
